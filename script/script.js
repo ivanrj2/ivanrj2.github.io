2b1f2e75c51eac85733daa66be1685952e55e660
ivanrj7j <ivanrj7j@gmail.com>
2024-09-16 10:53:23 +0530

    signup+login pages and minor changes

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ const heroSectionContents = [
         title2: "from 8 years",
         text: "Where to grow your business as a photographer: site or social media?",
         btnText: "Register",
-        btnLink: "/",
+        btnLink: "/signup.html",
         image: "assets/person.svg",
         imageWidth: "80%"
     },
@@ -16,7 +16,7 @@ const heroSectionContents = [
         title2: "69,420++",
         text: "Join 69,420 students who completed our coding course and thrive in their careers.",
         btnText: "Register",
-        btnLink: "/",
+        btnLink: "/signup.html",
         image: "assets/person.svg",
         imageWidth: "80%"
     },
@@ -25,7 +25,7 @@ const heroSectionContents = [
         title2: "Rock Solid Jobs",
         text: "40% of our students now work at FAANG companies after completing our course.",
         btnText: "Register",
-        btnLink: "/",
+        btnLink: "/signup.html",
         image: "assets/person.svg",
         imageWidth: "80%"
     }
